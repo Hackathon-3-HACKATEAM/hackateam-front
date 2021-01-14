2aeba1337cb65c47f3b5401fadbfdf9df4c58205
florentdesmarets <florent.desmarets@gmail.com>
2021-01-14 20:42:35 +0100

    fixed

--- src/components/SearchBar/Search.jsx
+++ src/components/SearchBar/Search.jsx
@@ -1,50 +1,106 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'; 
 import './Search.css'
 
+// Empty => 0 elements
+// Max => 5
 
 export default function Search() {
-    const [input, setInput] = useState("");
-    let test = [
-        {
-            name: "Search",
-            team: "hackathon2"
-        },
-        {
-            name: "Search2",
-            team: "hackathon1"
-        },
-    ];
+    const [cities, setCities] = useState([]);
+    const [filteredCities, setFilteredCities] = useState([]);
+    const [search, setSearch] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
-    const handleChange = (e) => {
-        e.preventDefault();
-        setInput(e.target.value);
 
-    };
-    if(input.length > 0){
-        test = test.filter((i) => {
-            return i.name.match(input);
+    // Equivalent (plus ou moins) du code dans le UseEffect Axios.
+    /* const getData = async () => {
+      const res = await axios.get('http://localhost:8000/api/cities');
+      if (res && res.data) {
+        const data = res.data;
+        setCities(data);
+      }
+    } */
+
+    useEffect(() => {
+      axios
+        .get('http://localhost:8000/api/cities', {
+        })
+        .then((res) => res.data)
+        .then((data) => {
+          setCities(data);
+        })
+        .catch((err) => {
+          let message;
+          if (err.response.status === 401) {
+            message = "You're not authorized to access these datas";
+          } else {
+            message = err.response.data.errorMessage;
+          }
+          console.log(message);
+          console.error(err);
         });
-    }
+    }, []);
 
+    useEffect(() => {
+      // Fonction qui s'auto appelle
+      (async () => {
+        
+        if (cities.length > 0) {
+          const data = await filterCities(search, cities);
+          console.log({data});
+          setFilteredCities(data);
+        }
+      })();
+      
+    }, [search]);
+
+    /**
+     * @param {*} text Texte venant de la barre de recherche
+     * @param {*} arr Collection de villes à filtrer
+     */
+    const filterCities = (text, arr) => {
+        const filteredArray = cities.filter(city => {
+          return city.city.toLowerCase().includes(search.toLowerCase());
+        });
+
+        filteredArray.sort((a, b) => {
+            if(a.city < b.city) { return -1; }
+            if(a.city > b.city) { return 1; }
+            return 0;
+        });
+        const newArr = filteredArray.slice(0, 5);
+  
+        // console.log({filteredArray});
+        return newArr;        
+    }
+    
     return(
         <div className="search-bar">
-            <input 
-            type="text" 
-            placeholder="Rechercher"
-            onChange={handleChange}
-            value={input}
+            <input
+              className="input-searchbar"
+              type="text" 
+              placeholder="Rechercher"
+              onChange={ e => setSearch(e.target.value)}
             />
 
-         
-            {test.map((player, index) => {
-                return ( 
-                   <div key={index}> 
-                    <ul> 
-                        <li> {player.name} - {player.team}</li>
-                    </ul>
+            { // Affichage conditionnel
+              isLoading && (
+                <span>Loading...</span>
+              )
+            }
+            
+            {  // Affichage conditionnel
+              search && !isLoading && (
+                filteredCities.map((city) => {
+                  return (
+                    <div className="search-results" key={city.id}> 
+                          <p className="results" > {city.city} </p>
                     </div>
-                )
-            })}
+                  )
+                })
+              )
+            }
+
         </div>
     );
 }
