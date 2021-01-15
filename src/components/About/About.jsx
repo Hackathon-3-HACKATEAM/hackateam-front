4b6675cdcf32e276a6566fc6074db44cf3840977
Sylvie-Saidi <sylvie.saidi22@gmail.com>
2021-01-15 10:05:25 +0100

    about finished

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,13 +6,15 @@ export default function About(){
         <div className="about-container">
           <h1 className="about-title">Vous aussi, rejoignez la communauté Comparateur Agricole !</h1>
           <img className="champ-ble"src="https://s1.1zoom.me/big3/811/Fields_Summer_Sky_Ear_436415.jpg"/>
-          <h2 className="about-title2">Une carte interractive mettant en avant nos collaborateurs</h2>
-          <p className="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <div className="about-content">
+          <h2 className="about-title2">Une carte interractive mettant en avant nos partenaires</h2>
+          <p className="about-text"> Grâce a cet outil, visualisez d'un simple coup d'oeil les agriculteurs qui nous font déjà confiance; ainsi que les acheteurs avec qui nous travaillons. La barre de recherche vous permettera d'afficher, pour une commune choisie et ses alentours, les entreprises travaillant avec nous.</p>
           <h2 className="about-title2">Une communauté soudée !</h2>
           <p className="about-text">Vous cherchez des collaborateurs de confiance qui aspirent aux mêmes idéaux que vous ?</p>
           
           <h2 className="about-title2">Nous aussi !</h2>
           <p className="about-text">Rejoignez-nous directement sur notre page ComparateurAgricole.com</p>
+          </div>
         </div>
     );
 }
